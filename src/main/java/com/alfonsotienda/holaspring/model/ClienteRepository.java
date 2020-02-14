67abb1b7f2f61c796d9fef124d4695487c26039e
lazaro-programacion <lazaro0089@gmail.com>
2020-02-14 12:11:14 +0100

    sdf

--- src/main/java/com/alfonsotienda/holaspring/model/ClienteRepository.java
+++ src/main/java/com/alfonsotienda/holaspring/model/ClienteRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Integer>{
 
- //   Iterable<Cliente> findByName(String name);
+ //   Iterable<Cliente> findByName(String name);   
     
 }
